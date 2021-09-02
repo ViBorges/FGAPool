5ba4ebcd431fd7e308860ec759a8bcd86cae6b8c
ViBorges <vbda123@gmail.com>
2021-09-01 22:48:01 -0300

    Sending completeRegistration data to database

--- app/src/main/java/com/tcc/fgapool/CompleteRegistration.kt
+++ app/src/main/java/com/tcc/fgapool/CompleteRegistration.kt
@@ -7,11 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 import com.tcc.fgapool.databinding.ActivityCompleteRegistrationBinding
 import com.tcc.fgapool.utils.CircleTransformation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class CompleteRegistration : AppCompatActivity(), AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
+class CompleteRegistration : AppCompatActivity(), AdapterView.OnItemSelectedListener,
+    CompoundButton.OnCheckedChangeListener {
 
     private lateinit var binding: ActivityCompleteRegistrationBinding
 
@@ -19,11 +25,23 @@ class CompleteRegistration : AppCompatActivity(), AdapterView.OnItemSelectedList
     private lateinit var carModel : EditText
     private lateinit var carColor : EditText
 
+    private var isDriver : Boolean = false
+
+    private lateinit var database:FirebaseDatabase
+    private lateinit var databaseRef:DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCompleteRegistrationBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        //Firebase user reference
+        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
+
+        //Database reference
+        database = Firebase.database
+        databaseRef = database.getReference("signup_info/"+currentFirebaseUser?.uid)
 
         //View objects
         val courseDropdown: Spinner = binding.courseDropdown
@@ -31,6 +49,9 @@ class CompleteRegistration : AppCompatActivity(), AdapterView.OnItemSelectedList
         val isDriverSwitch: SwitchCompat = binding.isDriver
         val profileName : TextView = binding.profileName
         val profilePicture : ImageView = binding.profilePicture
+        val sendButton : Button = binding.buttonSend
+        val registrationNumber : EditText = binding.editTextMatricula
+        val phoneNumber : EditText = binding.editTextPhone
         carPlate = binding.carPlate
         carModel = binding.carModel
         carColor = binding.carColor
@@ -52,13 +73,34 @@ class CompleteRegistration : AppCompatActivity(), AdapterView.OnItemSelectedList
             .transform(CircleTransformation())
             .into(profilePicture)
 
+        sendButton.setOnClickListener {
+            if (isDriver){
+                databaseRef.child("registrationNumber").setValue(registrationNumber.text.toString())
+                databaseRef.child("course").setValue(courseDropdown.selectedItem.toString())
+                databaseRef.child("gender").setValue(genderDropdown.selectedItem.toString())
+                databaseRef.child("phoneNumber").setValue(phoneNumber.text.toString())
+                databaseRef.child("isDriver").setValue(isDriver)
+                databaseRef.child("carPlate").setValue(carPlate.text.toString())
+                databaseRef.child("carModel").setValue(carModel.text.toString())
+                databaseRef.child("carColor").setValue(carColor.text.toString())
+                databaseRef.child("registrationComplete").setValue(true)
+            } else {
+                databaseRef.child("registrationNumber").setValue(registrationNumber.text.toString())
+                databaseRef.child("course").setValue(courseDropdown.selectedItem.toString())
+                databaseRef.child("gender").setValue(genderDropdown.selectedItem.toString())
+                databaseRef.child("phoneNumber").setValue(phoneNumber.text.toString())
+                databaseRef.child("isDriver").setValue(isDriver)
+                databaseRef.child("registrationComplete").setValue(true)
+            }
+
+        }
+
     }
 
     override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
-            // An item was selected. You can retrieve the selected item using
-            // parent.getItemAtPosition(pos)
-            parent.getItemAtPosition(pos).toString()
-
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        parent.getItemAtPosition(pos).toString()
     }
 
     override fun onNothingSelected(parent: AdapterView<*>) {
@@ -71,11 +113,15 @@ class CompleteRegistration : AppCompatActivity(), AdapterView.OnItemSelectedList
             carPlate.visibility = View.VISIBLE
             carModel.visibility = View.VISIBLE
             carColor.visibility = View.VISIBLE
+            isDriver = true
+
         } else {
 
             carPlate.visibility = View.GONE
             carModel.visibility = View.GONE
             carColor.visibility = View.GONE
+            isDriver = false
+
         }
 
     }
