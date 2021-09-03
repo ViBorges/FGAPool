1c0834a9eb3f87e78d8b82da69745a523b7a34b9
ViBorges <vbda123@gmail.com>
2021-09-03 18:44:23 -0300

    Checking user inputs on CompleteRegistration

--- app/src/main/java/com/tcc/fgapool/CompleteRegistration.kt
+++ app/src/main/java/com/tcc/fgapool/CompleteRegistration.kt
@@ -1,8 +1,10 @@
 package com.tcc.fgapool
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -75,16 +77,22 @@ class CompleteRegistration : AppCompatActivity(), CompoundButton.OnCheckedChange
             .into(profilePicture)
 
         sendButton.setOnClickListener {
-            if (isDriver) {
-                databaseRef.child("registrationNumber").setValue(registrationNumber.text.toString())
-                databaseRef.child("course").setValue(courseDropdown.selectedItem.toString())
-                databaseRef.child("gender").setValue(genderDropdown.selectedItem.toString())
-                databaseRef.child("phoneNumber").setValue(phoneNumber.text.toString())
-                databaseRef.child("isDriver").setValue(isDriver)
-                databaseRef.child("carPlate").setValue(carPlate.text.toString())
-                databaseRef.child("carModel").setValue(carModel.text.toString())
-                databaseRef.child("carColor").setValue(carColor.text.toString())
-                databaseRef.child("registrationComplete").setValue(true)
+            if (isDriver){
+                if (checkDriverInputs(registrationNumber, phoneNumber, carPlate, carModel, carColor, courseDropdown, genderDropdown)){
+                    alertDialog()
+                } else {
+                    databaseRef.child("registrationNumber").setValue(registrationNumber.text.toString())
+                    databaseRef.child("course").setValue(courseDropdown.selectedItem.toString())
+                    databaseRef.child("gender").setValue(genderDropdown.selectedItem.toString())
+                    databaseRef.child("phoneNumber").setValue(phoneNumber.text.toString())
+                    databaseRef.child("isDriver").setValue(isDriver)
+                    databaseRef.child("carPlate").setValue(carPlate.text.toString())
+                    databaseRef.child("carModel").setValue(carModel.text.toString())
+                    databaseRef.child("carColor").setValue(carColor.text.toString())
+                    databaseRef.child("registrationComplete").setValue(true)
+                }
+            } else if (checkPassengerInputs(registrationNumber, phoneNumber, courseDropdown, genderDropdown)){
+                alertDialog()
             } else {
                 databaseRef.child("registrationNumber").setValue(registrationNumber.text.toString())
                 databaseRef.child("course").setValue(courseDropdown.selectedItem.toString())
@@ -93,9 +101,50 @@ class CompleteRegistration : AppCompatActivity(), CompoundButton.OnCheckedChange
                 databaseRef.child("isDriver").setValue(isDriver)
                 databaseRef.child("registrationComplete").setValue(true)
             }
-
         }
+    }
 
+    private fun checkDriverInputs(registrationNumber:EditText, phoneNumber:EditText, carPlate:EditText,
+                                  carModel:EditText, carColor:EditText, courseDropdown:Spinner,
+                                  genderDropdown:Spinner) : Boolean{
+
+        return isLessThan(registrationNumber, 9) || isLessThan(phoneNumber, 11) ||
+                isLessThan(carPlate, 7) || isEmpty(carModel) || isEmpty(carColor) ||
+                spinnerAtZeroPos(courseDropdown) || spinnerAtZeroPos(genderDropdown)
+
+    }
+
+    private fun checkPassengerInputs(registrationNumber:EditText, phoneNumber:EditText,
+                            courseDropdown:Spinner, genderDropdown:Spinner) : Boolean{
+
+        return spinnerAtZeroPos(courseDropdown) || spinnerAtZeroPos(genderDropdown) ||
+                isLessThan(registrationNumber, 9) || isLessThan(phoneNumber, 11)
+
+
+    }
+
+    private fun isEmpty(editText:EditText) : Boolean {
+        return editText.text.isEmpty()
+    }
+
+    private fun isLessThan(editText:EditText, length:Int) : Boolean {
+        return editText.text.length < length
+    }
+
+    private fun spinnerAtZeroPos(spinner: Spinner):Boolean{
+        //Toast.makeText(this, spinner.selectedItemPosition.toString(), Toast.LENGTH_SHORT).show()
+        return spinner.selectedItemPosition == 0
+    }
+
+    private fun alertDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Alerta!")
+            .setMessage("Todos os campos são obrigatórios!")
+            .setCancelable(false)
+            .setPositiveButton(android.R.string.ok, DialogInterface.OnClickListener { dialog, which ->
+                dialog.cancel()
+            })
+        builder.show()
     }
 
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
