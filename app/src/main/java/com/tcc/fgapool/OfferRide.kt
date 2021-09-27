9b9abc5a5c0f2cb0baac634f541049112525981c
ViBorges <vbda123@gmail.com>
2021-09-27 20:36:28 -0300

    Completing user input fields on offerride

--- app/src/main/java/com/tcc/fgapool/OfferRide.kt
+++ app/src/main/java/com/tcc/fgapool/OfferRide.kt
@@ -2,28 +2,38 @@ package com.tcc.fgapool
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.widget.DatePicker
-import android.widget.EditText
-import android.widget.TimePicker
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.SwitchCompat
 import com.tcc.fgapool.databinding.ActivityOfferRideBinding
-import java.text.SimpleDateFormat
 import java.util.*
 
-lateinit var binding: ActivityOfferRideBinding
-lateinit var dateEditText: EditText
-lateinit var timeEditText: EditText
-var mDay = 0
-var mMonth = 0
-var mYear = 0
-var mHour = 0
-var mMinute = 0
-var savedHour = 0
-var savedMinute =0
+class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener, CompoundButton.OnCheckedChangeListener {
 
-class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+    lateinit var binding: ActivityOfferRideBinding
+    lateinit var dateEditText: EditText
+    lateinit var timeEditText: EditText
+    lateinit var originEditText: EditText
+    lateinit var destinationEditText: EditText
+    lateinit var routeEditText: EditText
+    lateinit var seatsEditText: EditText
+    lateinit var sameSexSwitch: SwitchCompat
+    lateinit var offerRideBtn: Button
+
+    var sameSexPassengers: Boolean = false
+
+    var mDay = 0
+    var mMonth = 0
+    var mYear = 0
+    var mHour = 0
+    var mMinute = 0
+    var savedHour = 0
+    var savedMinute = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOfferRideBinding.inflate(layoutInflater)
@@ -31,14 +41,23 @@ class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimeP
 
         dateEditText = binding.editTextDate
         timeEditText = binding.editTextTime
+        originEditText = binding.editTextOrigin
+        destinationEditText = binding.editTextDestination
+        routeEditText = binding.editTextRoute
+        seatsEditText = binding.editTextSeatsAmount
+        sameSexSwitch = binding.sameSexSwitch
+        offerRideBtn = binding.offerRideBtn
 
-        datePicker()
-        timePicker()
+        datePicker(dateEditText)
+        timePicker(timeEditText)
+        offerRide()
+
+        sameSexSwitch.setOnCheckedChangeListener(this)
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
 
-       dateEditText.setText(String.format("%02d/%02d/%d", dayOfMonth, month+1, year))
+        dateEditText.setText(String.format("%02d/%02d/%d", dayOfMonth, month + 1, year))
     }
 
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
@@ -49,7 +68,8 @@ class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimeP
         timeEditText.setText(String.format("%02d:%02d", hourOfDay, minute))
     }
 
-    private fun getCurrentTimeDate(){
+    private fun getCurrentTimeDate() {
+
         val cal = Calendar.getInstance(TimeZone.getDefault())
         mDay = cal.get(Calendar.DAY_OF_MONTH)
         mMonth = cal.get(Calendar.MONTH)
@@ -58,25 +78,56 @@ class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimeP
         mMinute = cal.get(Calendar.MINUTE)
     }
 
-    private fun datePicker(){
-        dateEditText.setOnClickListener {
+    private fun datePicker(editText: EditText) {
+        editText.setOnClickListener {
             getCurrentTimeDate()
 
             DatePickerDialog(this, this, mYear, mMonth, mDay).show()
         }
     }
 
-    private fun timePicker(){
-        timeEditText.setOnClickListener {
-
-            if (savedHour == 0 && savedMinute == 0){
+    private fun timePicker(editText: EditText) {
+        editText.setOnClickListener {
+            if (savedHour == 0 && savedMinute == 0) {
                 getCurrentTimeDate()
                 TimePickerDialog(this, this, mHour, mMinute, true).show()
             } else {
                 TimePickerDialog(this, this, savedHour, savedMinute, true).show()
             }
-
         }
+    }
+
+    private fun offerRide() {
+        offerRideBtn.setOnClickListener {
+            if (isEmpty(originEditText) || isEmpty(destinationEditText) || isEmpty(routeEditText)
+                || isEmpty(dateEditText) || isEmpty(timeEditText) || isEmpty(seatsEditText)
+            ) {
+                alertDialog()
+            } else {
+                Toast.makeText(this, "Enviado!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isEmpty(editText: EditText): Boolean {
+        return editText.text.isEmpty()
+    }
+
+    private fun alertDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Alerta!")
+            .setMessage("Todos os campos são obrigatórios!")
+            .setCancelable(false)
+            .setPositiveButton(
+                android.R.string.ok,
+                DialogInterface.OnClickListener { dialog, which ->
+                    dialog.cancel()
+                })
+        builder.show()
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        sameSexPassengers = isChecked
     }
 
 }
