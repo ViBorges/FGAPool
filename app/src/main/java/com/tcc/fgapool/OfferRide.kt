4a92b7ddc03c36d40db9495085ffbcf821799fe9
ViBorges <vbda123@gmail.com>
2021-09-27 22:10:10 -0300

    Send ride data to remote database

--- app/src/main/java/com/tcc/fgapool/OfferRide.kt
+++ app/src/main/java/com/tcc/fgapool/OfferRide.kt
@@ -3,41 +3,63 @@ package com.tcc.fgapool
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SwitchCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.tcc.fgapool.databinding.ActivityOfferRideBinding
+import com.tcc.fgapool.models.Ride
 import java.util.*
 
 class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     TimePickerDialog.OnTimeSetListener, CompoundButton.OnCheckedChangeListener {
 
-    lateinit var binding: ActivityOfferRideBinding
-    lateinit var dateEditText: EditText
-    lateinit var timeEditText: EditText
-    lateinit var originEditText: EditText
-    lateinit var destinationEditText: EditText
-    lateinit var routeEditText: EditText
-    lateinit var seatsEditText: EditText
-    lateinit var sameSexSwitch: SwitchCompat
-    lateinit var offerRideBtn: Button
+    private lateinit var binding: ActivityOfferRideBinding
+    private lateinit var dateEditText: EditText
+    private lateinit var timeEditText: EditText
+    private lateinit var originEditText: EditText
+    private lateinit var destinationEditText: EditText
+    private lateinit var routeEditText: EditText
+    private lateinit var seatsEditText: EditText
+    private lateinit var sameSexSwitch: SwitchCompat
+    private lateinit var offerRideBtn: Button
 
-    var sameSexPassengers: Boolean = false
+    private lateinit var database: FirebaseDatabase
+    private lateinit var databaseRef: DatabaseReference
+    private lateinit var uid: String
 
-    var mDay = 0
-    var mMonth = 0
-    var mYear = 0
-    var mHour = 0
-    var mMinute = 0
-    var savedHour = 0
-    var savedMinute = 0
+    private var sameSexPassengers: Boolean = false
+
+    private var mDay = 0
+    private var mMonth = 0
+    private var mYear = 0
+    private var mHour = 0
+    private var mMinute = 0
+    private var savedHour = 0
+    private var savedMinute = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOfferRideBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Firebase user reference
+        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
+        uid = currentFirebaseUser?.uid.toString()
+
+        //Database reference
+        database = Firebase.database
+        databaseRef = database.getReference("rides/").push()
+
 
         dateEditText = binding.editTextDate
         timeEditText = binding.editTextTime
@@ -104,7 +126,9 @@ class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
             ) {
                 alertDialog()
             } else {
-                Toast.makeText(this, "Enviado!", Toast.LENGTH_SHORT).show()
+                writeNewRide(originEditText.text.toString(), destinationEditText.text.toString(),
+                routeEditText.text.toString(), dateEditText.text.toString(), timeEditText.text.toString(),
+                seatsEditText.text.toString(), sameSexPassengers, true, uid)
             }
         }
     }
@@ -116,18 +140,53 @@ class OfferRide : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
     private fun alertDialog() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Alerta!")
-            .setMessage("Todos os campos são obrigatórios!")
+            .setMessage("Todos os campos devem ser preenchidos!")
             .setCancelable(false)
             .setPositiveButton(
-                android.R.string.ok,
-                DialogInterface.OnClickListener { dialog, which ->
-                    dialog.cancel()
-                })
+                android.R.string.ok
+            ) { dialog, _ ->
+                dialog.cancel()
+            }
         builder.show()
     }
 
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
         sameSexPassengers = isChecked
+    }
+
+    private fun writeNewRide(
+        origin: String,
+        destination: String,
+        route: String,
+        date: String,
+        time: String,
+        seatsAvailable: String,
+        sameSexPassengers: Boolean,
+        isActive: Boolean,
+        userId: String
+    ) {
+
+        val ride = Ride(
+            origin,
+            destination,
+            route,
+            date,
+            time,
+            seatsAvailable,
+            sameSexPassengers,
+            isActive,
+            userId
+        )
+
+        databaseRef.setValue(ride)
+            .addOnSuccessListener {
+                Toast.makeText(this, R.string.DB_on_send_success, Toast.LENGTH_SHORT).show()
+                finish()
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, R.string.DB_on_send_failure, Toast.LENGTH_SHORT).show()
+            }
     }
 
 }
---
+++ app/src/main/java/com/tcc/fgapool/models/Ride.kt
@@ -0,0 +1,13 @@
+package com.tcc.fgapool.models
+
+data class Ride(
+    val origin: String? = null,
+    val destination: String? = null,
+    val route: String? = null,
+    val date: String? = null,
+    val time: String? = null,
+    val seatsAvailable: String? = null,
+    val sameSexPassengers: Boolean? = null,
+    val isActive: Boolean? = null,
+    val userId: String? = null
+)
